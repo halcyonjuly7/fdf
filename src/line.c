c57526b88bd3a6dbfc2f2ff01508c4770a57e2a7
Halcyon abrah Ramirez <hramirez@e1z2r8p36.42.us.org>
2018-07-05 15:20:10 -0700

    norminetted edits

--- src/line.c
+++ src/line.c
@@ -7,6 +7,8 @@
 #include "rotate.h"
 #include <unistd.h>
 #include <stdlib.h>
+#define D_VARS  int h;int w;float x_m;float y_m;
+#define D_VARS1 t_struct_arr *cur_c;t_coords cds;t_coords *starting_point;
 
 
 void plot_linelow(t_mlx_holder* holder, t_coords* coords)
@@ -27,7 +29,8 @@ void plot_linelow(t_mlx_holder* holder, t_coords* coords)
     index = coords->x0;
     while(index < coords->x1)
     {
-        mlx_pixel_put(holder->mlx, holder->window, index, cont.y_axis, 0xffffff);
+        mlx_pixel_put(holder->mlx,holder->window, index, cont.y_axis,
+                      0xffffff);
         if (cont.distance > 0)
         {
             cont.y_axis = cont.y_axis + cont.y_index;
@@ -57,7 +60,8 @@ void plot_linehigh(t_mlx_holder* holder, t_coords *coords)
     index = coords->y0;
     while (index < coords->y1)
     {
-        mlx_pixel_put(holder->mlx, holder->window, cont.x_axis, index, 0xffffff);
+        mlx_pixel_put(holder->mlx, holder->window, cont.x_axis, index,
+                      0xffffff);
         if (cont.distance > 0)
         {
             cont.x_axis = cont.x_axis + cont.x_index;
@@ -138,105 +142,46 @@ void center_coordinates(t_coords *starting_point, t_sa_container* cont)
 }
 
 
-//void draw(t_mlx_holder* holder, t_sa_container* cont, int pix_size) {
-//    int height;
-//    int width;
-//    float x_mult;
-//    float y_mult;
-//    float x_tmp;
-//    float y_tmp;
-//    t_struct_arr *current_coord;
-//    t_data* next_coord;
-//    t_coords coords;
-//    t_coords *starting_point;
-//
-//    height = 0;
-//    x_mult = 50;
-//    y_mult = 50;
-//    x_tmp = 1;
-//    y_tmp = 1;
-//
-//    starting_point = find_starting_point(cont->height, cont->width, 50, 1500);
-////    center_coordinates(starting_point, cont);
-//    rotate(cont, starting_point, 90);
-//    center_coordinates(starting_point, cont);
-//    while (height  < cont->height) {
-//        width = 0;
-//        while ((width + 1)  < cont->width) {
-//            current_coord = cont->arr[height];
-//            coords.x0 = current_coord->arr[width]->x + (width * x_mult);
-//            coords.y0 = current_coord->arr[width]->y + (height * y_mult);
-//            coords.x1 = current_coord->arr[width + 1]->x + ((width + 1) * x_mult);
-//            coords.y1 = current_coord->arr[width + 1]->y + (height * y_mult);
-//            x_tmp = 50;
-//            y_tmp = 50;
-//            plot_linelow(holder, &coords);
-//            if (height + 1 < cont->height) {
-//                coords.x1 = coords.x0;
-//                coords.y1 = cont->arr[height + 1]->arr[width]->y + ((height + 1) * y_mult);
-//                plot_linehigh(holder, &coords);
-//                if (width + 1 == cont->width - 1) {
-//                    coords.x0 = current_coord->arr[width + 1]->x + ((width + 1) * x_tmp);
-//                    coords.y0 = current_coord->arr[width + 1]->y + (height * y_mult);
-//                    coords.x1 = coords.x0;
-//                    coords.y1 = cont->arr[height + 1]->arr[width]->y + ((height + 1) * y_mult);
-//                    plot_linehigh(holder, &coords);
-//
-//                }
-//            }
-//            width++;
-//        }
-//        x_tmp = 0;
-//        y_tmp = y_mult;
-//        height++;
-//    }
-//}
-
 void draw(t_mlx_holder* holder, t_sa_container* cont, int pix_size) {
-    int height;
-    int width;
-    float x_mult;
-    float y_mult;
-
-    t_struct_arr *current_coord;
-    t_coords coords;
+    int h;
+    int w;
+    float x_m;
+    float y_m;
+    t_struct_arr *cur_c;
+    t_coords cds;
     t_coords *starting_point;
 
-    height = 0;
-    x_mult = 50;
-    y_mult = 50;
-
-
+    h = 0;
+    x_m = 50;
+    y_m = 50;
     starting_point = find_starting_point(cont->height, cont->width, 50, 1500);
-    rotate(cont, starting_point, 180);
+    rotate(cont);
     center_coordinates(starting_point, cont);
-    while (height  < cont->height) {
-        width = 0;
-        while ((width + 1)  < cont->width) {
-            current_coord = cont->arr[height];
-            coords.x0 = current_coord->arr[width]->x + (width * x_mult);
-            coords.y0 = current_coord->arr[width]->y + (height * y_mult);
-            coords.x1 = current_coord->arr[width + 1]->x + ((width + 1) * x_mult);
-            coords.y1 = current_coord->arr[width + 1]->y + (height * y_mult);
-
-            plot_linelow(holder, &coords);
-            if (height + 1 < cont->height) {
-                coords.x1 = cont->arr[height + 1]->arr[width]->x + (width * x_mult);
-                coords.y1 = cont->arr[height + 1]->arr[width]->y + ((height + 1) * y_mult);
-
-                plot_linehigh(holder, &coords);
-                if (width + 1 == cont->width - 1) {
-                    coords.x0 = current_coord->arr[width + 1]->x + ((width + 1) * x_mult);
-                    coords.y0 = current_coord->arr[width + 1]->y + (height * y_mult);
-                    coords.x1 = cont->arr[height + 1]->arr[width + 1]->x + ((width + 1) * x_mult);
-                    coords.y1 = cont->arr[height + 1]->arr[width + 1]->y + ((height + 1) * y_mult);
-                    plot_linehigh(holder, &coords);
-
+    while (h  < cont->height) {
+        w = 0;
+        while ((w + 1)  < cont->width) {
+            cur_c = cont->arr[h];
+            cds.x0 = cur_c->arr[w]->x + (w * x_m);
+            cds.y0 = cur_c->arr[w]->y + (h * y_m);
+            cds.x1 = cur_c->arr[w + 1]->x + ((w + 1) * x_m);
+            cds.y1 = cur_c->arr[w + 1]->y + (h * y_m);
+            plot_linelow(holder, &cds);
+            if (h + 1 < cont->height) {
+                cds.x1 = cont->arr[h + 1]->arr[w]->x + (w * x_m);
+                cds.y1 = cont->arr[h + 1]->arr[w]->y + ((h + 1) * y_m);
+                plot_linehigh(holder, &cds);
+                if (w + 1 == cont->width - 1) {
+                    cds.x0 = cur_c->arr[w + 1]->x + ((w + 1) * x_m);
+                    cds.y0 = cur_c->arr[w + 1]->y + (h * y_m);
+                    cds.x1 = cont->arr[h + 1]->arr[w + 1]->x + ((w + 1) * x_m);
+                    cds.y1 = cont->arr[h + 1]->arr[w + 1]->y + ((h + 1) * y_m);
+                    plot_linehigh(holder, &cds);
                 }
             }
-            width++;
+            w++;
         }
-        height++;
+        h++;
     }
 }
+
 
